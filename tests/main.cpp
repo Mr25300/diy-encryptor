20829f44f1dfa5ac9175cf2f5a747bb4e8bbe3eb
Sebastian Cooper-Giannotta <109829617+Mr25300@users.noreply.github.com>
2026-07-25 18:29:07 -0400

    Added tests for PBKDF2

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,22 +1,16 @@
 #include <hashes/sha256.hpp>
 #include <prfs/hmac.hpp>
+#include <kdfs/pbkdf2.hpp>
 #include <utils/bytes.hpp>
 
 #include <string>
+#include <string_view>
 #include <cassert>
 #include <stdexcept>
 #include <iostream>
+#include <iomanip>
 
-template <typename C1, typename C2>
-bool areEqual(C1 bytes1, C2 bytes2) {
-    if (bytes1.size() != bytes2.size()) return false;
-
-    for (std::size_t i{}; i < bytes1.size(); ++i) {
-        if (bytes1[i] != bytes2[i]) return false;
-    }
-
-    return true;
-}
+void runTest(std::string_view suite, std::string_view name)
 
 bytes::ByteVec toBytes(std::string_view str, std::size_t repeatCount = 1) {
     bytes::ByteVec bytes;
@@ -49,20 +43,21 @@ bytes::ByteVec parseHexString(std::string_view str) {
     return bytes;
 }
 
-// template <typename C>
-// void printBytes(const C& bytes, std::ostream& output) {
-//     output << std::hex << std::setw(2) << std::setfill('0');
-//
-//     for (std::uint8_t b : bytes) {
-//         output << static_cast<int>(b);
-//     }
-//
-//     output << std::dec;
-// }
+template <typename C>
+void printBytes(const C& bytes, std::ostream& output) {
+    output << std::hex << std::setw(2) << std::setfill('0');
+
+    for (std::uint8_t b : bytes) {
+        output << static_cast<int>(b);
+    }
+
+    output << std::dec;
+}
 
 void testSHA256(const bytes::ByteVec& input, const bytes::ByteVec& expected) {
     static std::size_t testNum{1};
-    static hashes::SHA256 sha256{};
+
+    hashes::SHA256 sha256{};
     bytes::ByteArr<32> output{sha256.compute(input)};
 
     if (areEqual(output, expected)) {
@@ -76,14 +71,30 @@ void testSHA256(const bytes::ByteVec& input, const bytes::ByteVec& expected) {
 
 void testHMAC(const bytes::ByteVec& key, const bytes::ByteVec& data, const bytes::ByteVec& expected) {
     static std::size_t testNum{1};
-    static hashes::SHA256 sha256{};
-    static prfs::HMAC<64, 32> hmac{sha256};
+
+    prfs::HMAC<64, 32> hmac{hashes::SHA256{}};
     bytes::ByteArr<32> output{hmac.compute(key, data)};
 
     if (areEqual(output, expected)) {
         std::cout << "HMAC passed test " << testNum << '\n';
     } else {
         std::cerr << "HMAC failed test " << testNum << '\n';
+    }
+
+    ++testNum;
+}
+
+template <std::size_t DKeySize>
+void testPBKDF2(const bytes::ByteVec& key, const bytes::ByteVec& salt, std::size_t iterations, const bytes::ByteVec& expected) {
+    static std::size_t testNum{1};
+
+    kdfs::PBKDF2<32, DKeySize> pbkdf2{prfs::HMAC<64, 32>{hashes::SHA256{}}, iterations};
+    bytes::ByteArr<DKeySize> output{pbkdf2.compute(key, salt)};
+
+    if (areEqual(output, expected)) {
+        std::cout << "PBKDF2 passed test " << testNum << '\n';
+    } else {
+        std::cerr << "PBKDF2 failed test " << testNum << '\n';
     }
 
     ++testNum;
@@ -115,10 +126,10 @@ int main() {
         parseHexString("cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1")
     );
 
-    testSHA256(
-        toBytes("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno", 16777216),
-        parseHexString("50e72a0e26442fe2552dc3938ac58658228c0cbfb1d2ca872ae435266fcd055e")
-    );
+    // testSHA256(
+    //     toBytes("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno", 16777216),
+    //     parseHexString("50e72a0e26442fe2552dc3938ac58658228c0cbfb1d2ca872ae435266fcd055e")
+    // );
 
     testHMAC(
         bytes::ByteVec(20, 0x0b),
@@ -154,6 +165,69 @@ int main() {
         bytes::ByteVec(131, 0xaa),
         toBytes("This is a test using a larger than block-size key and a larger than block-size data. The key needs to be hashed before being used by the HMAC algorithm."),
         parseHexString("9b09ffa71b942fcb27635fbcd5b0e944bfdc63644f0713938a7f51535c3a35e2")
+    );
+
+    testPBKDF2<20>(
+        toBytes("password"),
+        toBytes("salt"),
+        1,
+        parseHexString("120fb6cffcf8b32c43e7225256c4f837a86548c9")
+    );
+
+    testPBKDF2<20>(
+        toBytes("password"),
+        toBytes("salt"),
+        2,
+        parseHexString("ae4d0c95af6b46d32d0adff928f06dd02a303f8e")
+    );
+
+    testPBKDF2<20>(
+        toBytes("password"),
+        toBytes("salt"),
+        4096,
+        parseHexString("c5e478d59288c841aa530db6845c4c8d962893a0")
+    );
+
+    // testPBKDF2<20>(
+    //     toBytes("password"),
+    //     toBytes("salt"),
+    //     16777216,
+    //     parseHexString("cf81c66fe8cfc04d1f31ecb65dab4089f7f179e8")
+    // );
+
+    testPBKDF2<25>(
+        toBytes("passwordPASSWORDpassword"),
+        toBytes("saltSALTsaltSALTsaltSALTsaltSALTsalt"),
+        4096,
+        parseHexString("348c89dbcbd32b2f32d814b8116e84cf2b17347ebc1800181c")
+    );
+
+    testPBKDF2<16>(
+        toBytes(std::string("pass\0word", 9)),
+        toBytes(std::string("sa\0lt", 5)),
+        4096,
+        parseHexString("89b69d0516f829893c696226650a8687")
+    );
+
+    testPBKDF2<128>(
+        toBytes("passwd"),
+        toBytes("salt"),
+        1,
+        parseHexString("55ac046e56e3089fec1691c22544b605f94185216dde0465e68b9d57c20dacbc49ca9cccf179b645991664b39d77ef317c71b845b1e30bd509112041d3a19783c294e850150390e1160c34d62e9665d659ae49d314510fc98274cc79681968104b8f89237e69b2d549111868658be62f59bd715cac44a1147ed5317c9bae6b2a")
+    );
+
+    testPBKDF2<128>(
+        toBytes("Password"),
+        toBytes("NaCl"),
+        80000,
+        parseHexString("4ddcd8f60b98be21830cee5ef22701f9641a4418d04c0414aeff08876b34ab56a1d425a1225833549adb841b51c9b3176a272bdebba1d078478f62b397f33c8d62aae85a11cdde829d89cb6ffd1ab0e63a981f8747d2f2f9fe5874165c83c168d2eed1d2d5ca4052dec2be5715623da019b8c0ec87dc36aa751c38f9893d15c3")
+    );
+
+    testPBKDF2<256>(
+        toBytes("Password"),
+        toBytes(std::string("sa\0lt", 5)),
+        4096,
+        parseHexString("436c82c6af9010bb0fdb274791934ac7dee21745dd11fb57bb90112ab187c495ad82df776ad7cefb606f34fedca59baa5922a57f3e91bc0e11960da7ec87ed0471b456a0808b60dff757b7d313d4068bf8d337a99caede24f3248f87d1bf16892b70b076a07dd163a8a09db788ae34300ff2f2d0a92c9e678186183622a636f4cbce15680dfea46f6d224e51c299d4946aa2471133a649288eef3e4227b609cf203dba65e9fa69e63d35b6ff435ff51664cbd6773d72ebc341d239f0084b004388d6afa504eee6719a7ae1bb9daf6b7628d851fab335f1d13948e8ee6f7ab033a32df447f8d0950809a70066605d6960847ed436fa52cdfbcf261b44d2a87061")
     );
 
     return 0;
